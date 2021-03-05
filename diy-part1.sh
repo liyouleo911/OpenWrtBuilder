3dfbb6199bea4cfd8b8e395663eb76a12645d2b2
Leo <liyouleo911@msn.com>
2021-03-05 16:36:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,15 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package-temp
+mv -f package-temp/luci-app-openclash package/lean/
+rm -rf package-temp
+
+# Change default ip
+sed -i 's/192.168.1.1/192.168.21.130/g' package/base-files/files/bin/config_generate
